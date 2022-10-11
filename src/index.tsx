9b6b87d2f8dc6ac0918d0030e8681d07f8a8264a
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-10-12 01:15:22 +0530

    Coomit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
-
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    rootElement
+  );
+}
