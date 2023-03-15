9955795d2618c11de025e96c18ddae4aa8eff044
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2023-03-15 12:44:28 +0530

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
-export const API_TESTING_URL = "https://pediageektest.azurewebsites.net"
+export const API_TESTING_URL = "https://pediageekapi.azurewebsites.net"
 // export const API_TESTING_URL = "http://localhost:5000"
